765a14b528ba986a259b3840491541e91340f2c3
Philipp Joram <philipp.joram@tu-dresden.de>
2019-07-19 18:25:46 +0200

    Fix metricq version (until it has an actual one)

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setup(
       [console_scripts]
       metricq-sink-nsca=metricq_sink_nsca:main
       """,
-    install_requires=["click", "click-log", "metricq"],
+    install_requires=["click", "click-log", "metricq==0.0"],
 )
