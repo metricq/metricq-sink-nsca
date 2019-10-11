6bcc5d36993f6d1b0107eef51936759cf34bdf35
Philipp Joram <philipp.joram@tu-dresden.de>
2019-10-11 13:42:18 +0200

    StateTransitionHistory: add explicit epoch and docs

--- metricq_sink_nsca/state_cache.py
+++ metricq_sink_nsca/state_cache.py
@@ -19,7 +19,8 @@
 # along with metricq.  If not, see <http://www.gnu.org/licenses/>.
 
 from typing import Dict, Optional, Set, List, Iterable
-from dataclasses import dataclass
+from dataclasses import dataclass, field as dataclass_field
+from bisect import bisect_left
 
 from metricq.types import Timestamp, Timedelta
 
@@ -29,58 +30,149 @@ from .logging import get_logger
 logger = get_logger(__name__)
 
 
-@dataclass
+@dataclass(order=True)
 class StateTransition:
+    """A state transition where up until ``time``, a metric resided in state
+    ``state``.
+
+    For any metric ``M``, we observe a series of state transitions.  Suppose
+    there are two consecutive state transitions ``t1`` and ``t2``, i.e.
+
+        ``t1.time < t2.time``
+
+    We say that these transitions have _"last semantics"_, which means that at
+    any time ``t1.time < t <= t2.time``, values of ``M`` were in state
+    ``t2.state``.  In other words, the metric resides in some state ``t1.state``
+    up until the time the new transition ``t2`` happens, at which point we know
+    that ``t2.state`` occupied the _last time interval_.
+    """
+
     time: Timestamp
-    state: State
+    state: State = dataclass_field(compare=False, default=State.UNKNOWN)
 
 
 class StateTransitionHistory:
     def __init__(self, time_window: Optional[Timedelta]):
+        """A history of state transitions for some metric, spanning at most a
+        duration of ``time_window``.
+
+        It contains a point in time called 'epoch' that signifies when the state
+        that the first transition switched from was entered.  In a graph (t[0]
+        is the first transition, t[1] the second etc.):
+
+                  ┌┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄╮    ⋯
+                  │  t[0].state  │     t[1].state     │
+           ⋯──────┴──────────────┴────────────────────┴────────────→
+                epoch        t[0].time            t[1].time       time
+        """
+
+        # The list of state transitions for this metric.
         self._transitions: List[StateTransition] = list()
-        self._time_window: Optional[Timedelta] = time_window
+
+        # The point in time at which we assume the metric entered the state
+        # given by the first transition ``self._transitions[0]``, if present.
+        # This is necessary as transitions have last semantics, and we otherwise
+        # had to assume that it was in this state since forever (which skews
+        # statistics significantly, as you can imagine).
+        self._epoch: Optional[Timestamp] = None
+
+        # Use a sensible default time window for keeping past transitions.
+        self._time_window: Timedelta
+        if time_window is None:
+            self._time_window = Timedelta.from_s(30)
+        else:
+            if time_window.ns > 0:
+                self._time_window = time_window
+            else:
+                raise ValueError(
+                    "State transition history time window must be a positive duration"
+                )
 
     def insert(self, time: Timestamp, state: State):
-        transition = StateTransition(time, state)
-        self._transitions.append(transition)
-
-        # prune old transitions
-        if self._time_window is not None:
-            for i in range(len(self._transitions[:-1])):
-                if self._transitions[i + 1].time + self._time_window >= time:
-                    del self._transitions[:i]
-                    break
+        """Insert a transition that happened at ``time``, away from state
+        ``state``, towards some other, unknown state.
+        """
+        if self._epoch is None:
+            # If this is the first transition ever, we use it as an anchor point
+            # in time and discard the state the metric came from.  When the next
+            # transition is inserted, we know exactly how long the given state
+            # was valid for.
+            self._epoch = time
+            return
         else:
-            del self._transitions[:-1]
+            transition = StateTransition(time, state)
+            if self._transitions and time <= self._transitions[-1].time:
+                raise ValueError(
+                    f"Inserted times of state transitions must be strictly increasing "
+                    f"({time.posix_ns} <= {self._transitions[-1].time.posix_ns})"
+                )
+            self._transitions.append(transition)
 
-    def current_time_window(self) -> Optional[Timedelta]:
-        if not self._transitions:
-            return None
+        # Prune any transitions that happened outside of the time window in
+        # which we are interested in, with respect to the newly inserted
+        # transition.
+        #
+        # We use a binary search to find the index ``i`` of the first transition
+        # ``t[i]`` that happened exactly at or after the cutoff.  We discard it
+        # (and any older transitions), but keep its time as the new epoch.
+        # This way we make sure that we never keep a history of transitions
+        # spanning more than ``self._time_window``.
+        history_cutoff = time - self._time_window
+        if self._epoch > history_cutoff:
+            # Transitions span less then ``self._time_window``, no need to
+            # prune any of them.
+            return
         else:
-            return self._transitions[-1].time - self._transitions[0].time
+            i = bisect_left(self._transitions, StateTransition(time=history_cutoff))
+            # The newly inserted transition at index ``len(self._transitions) - 1``
+            # always happened after the cutoff, since ``self._time_window`` is
+            # positive.  Therefore we will _always_ find a matching transition
+            # within our history.
+            assert i < len(self._transitions)
+
+            # Save the new epoch and discard any transitions that are too old.
+            self._epoch = self._transitions[i].time
+            self._transitions = self._transitions[i + 1 :]
 
     def state_prevalences(self) -> Optional[Dict[State, float]]:
-        time_window = self.current_time_window()
-        if time_window is None or time_window.ns == 0:
+        """Return a ``dict`` where for each state, the share of time that a
+        metric was in this state is a ``float`` between ``0.0`` and ``1.0``.
+        """
+        # We might only calculate prevalences of states if we already set an
+        # epoch and there exists at least one transition.
+        if self._epoch is None or len(self._transitions) < 1:
             return None
 
-        assert self._transitions
-        prevalences = {state: Timedelta(0) for state in State}
-        last_transition = self._transitions[0]
-        for transition in self._transitions[1:]:
-            prevalences[transition.state] += transition.time - last_transition.time
-            last_transition = transition
+        # Determine the first timestamp for calculating the cumulative duration
+        # of each state.  Make sure it is at most self._time_window in the past,
+        # wrt. the most recent transition in this history.
+        latest_transition = self._transitions[-1]
+        oldest_transition_time = max(
+            latest_transition.time - self._time_window, self._epoch
+        )
+
+        total_duration: Timedelta = latest_transition.time - oldest_transition_time
+
+        cumulative_durations = {state: Timedelta(0) for state in State}
+
+        prev_time: Timestamp = oldest_transition_time
+        current: StateTransition
+        for current in self._transitions:
+            cumulative_durations[current.state] += current.time - prev_time
+            prev_time = current.time
 
         try:
+            # Return the prevalence of each state as a percentage of the total
+            # time spanned by all transitions.
             return {
-                state: duration.ns / time_window.ns
-                for state, duration in prevalences.items()
+                state: duration.ns / total_duration.ns
+                for state, duration in cumulative_durations.items()
             }
         except ZeroDivisionError:
             return None
 
     def __repr__(self):
-        return f"{type(self).__name__}(window={self._time_window}, current_window={self.current_time_window()}, transitions={self._transitions})"
+        return f"{type(self).__name__}(window={self._time_window}, transitions={self._transitions})"
 
 
 class StateCache:
