7482616fbf113f550c2396b0d9af8246eef2d8d2
Philipp Joram <philipp.joram@tu-dresden.de>
2020-09-11 17:59:22 +0200

    Move from `setup.py` to `setup.cfg`

--- setup.py
+++ setup.py
@@ -1,34 +1,3 @@
-# metricq-sink-nsca
-# Copyright (C) 2019 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
-#
-# All rights reserved.
-#
-# This file is part of metricq.
-#
-# metricq is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# metricq is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with metricq.  If not, see <http://www.gnu.org/licenses/>.
 from setuptools import setup
 
-setup(
-    name="metricq_sink_nsca",
-    version="1.5",
-    author="TU Dresden",
-    python_requires=">=3.7",
-    packages=["metricq_sink_nsca"],
-    scripts=[],
-    entry_points="""
-      [console_scripts]
-      metricq-sink-nsca=metricq_sink_nsca:main
-      """,
-    install_requires=["click", "click-log", "metricq~=1.0"],
-)
+setup()
