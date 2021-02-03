ced83eb84cbde8cfc0fb39ec7a9a610d1d3ee835
Philipp Joram <philipp.joram@tu-dresden.de>
2021-02-03 22:09:59 +0100

    test(check): `ignore_update_errors` should not generate reports

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Dict
 
 import pytest
 from metricq import Timedelta, Timestamp
@@ -7,16 +8,28 @@ from metricq_sink_nsca.check import Check, TvPair
 from metricq_sink_nsca.report_queue import Report, ReportQueue
 from metricq_sink_nsca.state import State
 
+from tests.conftest import Ticker
+
 
 @pytest.fixture
-def check():
-    return Check(
+def check_default_args() -> Dict[str, Any]:
+    return dict(
         name="test",
         metrics=["foo"],
         report_queue=ReportQueue(),
         value_constraints=None,
         resend_interval=Timedelta.from_s(60),
     )
+
+
+@pytest.fixture
+def check(check_default_args):
+    return Check(**check_default_args)
+
+
+@pytest.fixture
+def check_with_ignore_update_errors(check_default_args: dict) -> Check:
+    return Check(**check_default_args, ignore_update_errors=True)
 
 
 def test_check_internal_exception_report(check):
@@ -81,3 +94,33 @@ def test_check_internal_exception_log_entry(check, caplog):
             "Unhandled exception" in message
             for logger, level, message in caplog.record_tuples
         )
+
+
+def test_check_ignore_update_error(
+    check_with_ignore_update_errors: Check,
+    ticker: Ticker,
+    caplog: pytest.LogCaptureFixture,
+):
+    """Assert that a check that has `ignore_update_errors` set will not generate CRITICAL reports for non-monotonic metrics."""
+    check = check_with_ignore_update_errors
+    report_queue = check._report_queue
+
+    epoch = next(ticker)
+
+    # Make sure the internal state cache has an epoch set
+    check.check("foo", tv_pairs=[TvPair(epoch, 0)])
+    assert report_queue._queue.get_nowait().state == State.OK
+
+    # If ignore_update_errors is set, tv_pairs with duplicate/past timestamps should be ignored.
+    with caplog.at_level(logging.ERROR):
+        duplicate = TvPair(next(ticker), 0)
+        check.check("foo", tv_pairs=[duplicate, duplicate])
+
+        # Make sure a log message at level ERROR is generated
+        assert any(
+            "duplicate" in message and level == logging.ERROR
+            for logger, level, message in caplog.record_tuples
+        )
+
+        # Assert that no report was generated for these values
+        assert check._report_queue._queue.empty()
