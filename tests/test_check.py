3e016a261c4c4a004ce4a28b0effd37c86b2d272
Philipp Joram <philipp.joram@tu-dresden.de>
2021-06-24 15:33:45 +0200

    test(check): Test parsing Check from configuration

--- tests/test_check.py
+++ tests/test_check.py
@@ -3,19 +3,110 @@ import logging
 import pytest
 from metricq import Timedelta, Timestamp
 
-from metricq_sink_nsca.check import Check, TvPair
+from metricq_sink_nsca.check import Check, CheckConfig, TvPair
+from metricq_sink_nsca.override import Overrides
 from metricq_sink_nsca.report_queue import Report, ReportQueue
 from metricq_sink_nsca.state import State
+from metricq_sink_nsca.value_check import ValueCheckConfig
+
+DEFAULT_RESEND_INTERVAL = Timedelta.from_s(60)
+
+
+@pytest.fixture
+def config_minimal() -> CheckConfig:
+    return {"metrics": ["test.foo", "test.bar"]}
+
+
+@pytest.fixture
+def check_kwargs():
+    return {
+        "name": "test",
+        "overrides": Overrides.empty(),
+        "report_queue": ReportQueue(),
+        "resend_interval": DEFAULT_RESEND_INTERVAL,
+    }
+
+
+def test_check_config_minimal(config_minimal: CheckConfig, check_kwargs):
+    check = Check.from_config(
+        config=config_minimal,
+        **check_kwargs,
+    )
+
+    assert check.config == config_minimal
+    assert "test.foo" in check.metrics()
+    assert "test.bar" in check.metrics()
+    assert not check._has_timeout_checks()
+    assert not check._has_value_checks()
+
+
+def test_check_config_value_check_minimal(config_minimal: CheckConfig, check_kwargs):
+    config = config_minimal.copy()
+    config["warning_above"] = 0.0
+
+    check = Check.from_config(config=config, **check_kwargs)
+
+    assert check._timeout is None
+    assert not check._has_timeout_checks()
+    assert check._has_value_checks()
+
+
+def test_check_config_timeout_check_minimal(config_minimal: CheckConfig, check_kwargs):
+    """Specifying a `timeout` in the configuration creates active `TimeoutCheck`s for this `Check`."""
+    config = config_minimal.copy()
+    config["timeout"] = "30s"
+
+    check = Check.from_config(config=config, **check_kwargs)
+
+    assert check._timeout == Timedelta.from_s(30)
+    assert check._has_timeout_checks()
+    assert not check._has_value_checks()
+
+
+def test_check_config_resend_interval(config_minimal: CheckConfig, check_kwargs):
+    """A `resend_interval` in a check configuration overrides the global default."""
+    config = config_minimal.copy()
+    config["resend_interval"] = "10s"  # override
+    check_kwargs["resend_interval"] = "60s"  # global default
+
+    check = Check.from_config(config=config, **check_kwargs)
+
+    assert check._resend_interval == Timedelta.from_s(10)
+
+
+def test_check_config_full(config_minimal: CheckConfig, check_kwargs):
+    value_check_config = ValueCheckConfig(
+        critical_above=100.0,
+        warning_above=80.0,
+        warning_below=20.0,
+        critical_below=0.0,
+        ignore=[42.0],
+    )
+    config = CheckConfig(
+        metrics=["test.foo", "test.bar"],
+        timeout="30s",
+        **value_check_config,
+    )
+
+    check = Check.from_config(config=config, **check_kwargs)
+
+    assert check._has_timeout_checks()
+    assert check._has_value_checks()
 
 
 @pytest.fixture
 def check():
     return Check(
         name="test",
-        metrics=["foo"],
+        metrics={"foo"},
         report_queue=ReportQueue(),
-        value_constraints=None,
+        value_check=None,
+        timeout=None,
+        plugins={},
+        transition_debounce_window=None,
+        transition_postprocessor=None,
         resend_interval=Timedelta.from_s(60),
+        config={},
     )
 
 
